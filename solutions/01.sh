8ce34665a334e4550062ba9f7386bb5b8e37c36c
vgillo <vivien.gillo@gmail.com>
2021-09-16 11:02:40 +0200

    answer 18

---
+++ solutions/01.sh
@@ -0,0 +1,4 @@
+#! bin/bash
+What is the operating system of our server?
+Response : Linux sdd-ctf 4.19.0-17-cloud-amd64 1 SMP Debian 4.19.194-3 (2021-07-18) x86_64 GNU/Linux
+Code : $ uname
---
+++ solutions/18.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Line number of last instance of croissant in french
+#Response : 75930
+ssh ctf grep -n -x croissant french
